b3c20fbe1ad7889bc06307139a6f2e665a03d54a
wnsgh <junho767@naver.com>
2025-03-15 18:03:57 +0900

    chore: backend 개발환경 세팅

---
+++ backend/src/test/java/com/kop/morning/MorningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kop.morning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
